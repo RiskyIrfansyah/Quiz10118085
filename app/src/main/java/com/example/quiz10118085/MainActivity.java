a39bf70767d67ea350e6a80cab8993d616123894
Risky Irfansyah <kikiirfansyah39@gmail.com>
2021-05-29 10:34:55 +0700

    Initial commit

---
+++ app/src/main/java/com/example/quiz10118085/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.quiz10118085;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //Tanggal pengerjaan : 29 Mei 2021
+    //Nama : Risky Irfansyah
+    //NIM : 1011805
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
